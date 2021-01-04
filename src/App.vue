da26178b7674993113b1d0f918d757eebd7a750a
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-01-04 16:39:10 -0700

    Import TheHeader component directly in App

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/" />
-    </div>
+    <TheHeader />
+    <router-link to="/" />
     <router-view/>
   </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue'
 
+export default {
+  components: { TheHeader }
+}
 </script>
 
 <style lang="scss">
