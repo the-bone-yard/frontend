5f5f0e1003ff89b3b798fae4c2498535337a973e
Stacy Potten <stacylynnaz@gmail.com>
2021-01-04 15:45:00 -0700

    Add basic html layout

---
+++ src/views/Results.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <button @click="selectView">List View</button>
+    <button @click="selectView">Map View</button>
+    <results-list v-if=""></results-list>
+    <results-map></results-map>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      view: 'list',
+    };
+  },
+  methods: {
+    // selectView() {
+    // }
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
