a3cf0bf9a4fb7c3230af107a8eeb7abc42a6cea5
Nathan Darrington <npdarrington@icloud.com>
2021-01-05 14:59:26 -0700

    Added GmapMap for ResultsMap using test data

--- src/components/ResultsMap.vue
+++ src/components/ResultsMap.vue
@@ -1,5 +1,19 @@
 <template>
-	
+	<GmapMap
+		:center="{ lat: 40.0562161, lng: -105.0465427 }"
+		:zoom="14"
+		map-type-id="terrain"
+		style="width: 100%; height: 50vh"
+	>
+		<GmapMarker
+			:key="index"
+			v-for="(m, index) in markers"
+			:position="m.geomerty"
+			:clickable="true"
+			:draggable="false"
+			@click="testClickEvent(m.full_name)"
+		/>
+	</GmapMap>
 </template>
 
 <script>
