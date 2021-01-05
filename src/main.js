c370782f0c39a3e886de4d1fa0cb593d5212505b
Nathan Darrington <npdarrington@icloud.com>
2021-01-05 15:28:23 -0700

    Change key from being hard coded to env file declaration

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import * as VueGoogleMaps from 'vue2-google-maps';
 
 Vue.use(VueGoogleMaps, {
 	load: {
-		key: 'AIzaSyBsgmotDogNtLcgNV00LktP-WjYfLTKkAs',
+		key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY,
 		libraries: 'places', // This is required if you use the Autocomplete plugin
 		// OR: libraries: 'places,drawing'
 		// OR: libraries: 'places,drawing,visualization'
