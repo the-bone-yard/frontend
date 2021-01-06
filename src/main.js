24790438d91fdb9eef83afca7996d3c0133498d2
Nathan Darrington <npdarrington@icloud.com>
2021-01-05 19:45:11 -0700

    Removed filler comment code for using VueGoogleMaps

--- src/main.js
+++ src/main.js
@@ -10,26 +10,8 @@ import * as VueGoogleMaps from 'vue2-google-maps';
 Vue.use(VueGoogleMaps, {
 	load: {
 		key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY,
-		libraries: 'places', // This is required if you use the Autocomplete plugin
-		// OR: libraries: 'places,drawing'
-		// OR: libraries: 'places,drawing,visualization'
-		// (as you require)
-
-		//// If you want to set the version, you can do so:
-		// v: '3.26',
+		libraries: 'places',
 	},
-
-	//// If you intend to programmatically custom event listener code
-	//// (e.g. `this.$refs.gmap.$on('zoom_changed', someFunc)`)
-	//// instead of going through Vue templates (e.g. `<GmapMap @zoom_changed="someFunc">`)
-	//// you might need to turn this on.
-	// autobindAllEvents: false,
-
-	//// If you want to manually install components, e.g.
-	//// import {GmapMarker} from 'vue2-google-maps/src/components/marker'
-	//// Vue.component('GmapMarker', GmapMarker)
-	//// then disable the following:
-	// installComponents: true,
 });
 
 new Vue({
