c0aec45fd54b73334bab6e2dcef417c079355c69
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-01-06 11:23:17 -0700

    Create basic template elements

--- src/components/ResultsItemDetails.vue
+++ src/components/ResultsItemDetails.vue
@@ -1,5 +1,15 @@
 <template>
-  
+  <section>
+    <button class='button-save-park'>SAVE</button>
+    <h1 class='detail-descriptor'>Destination: </h1>
+    <h1 class='detail-descriptor'>Description: </h1>
+    <h1 class='detail-descriptor'>Directions: </h1>
+    <h2>Not the right park for your pup?</h2>
+    <article>
+      <button>search again</button>
+      <button>home</button>
+    </article>
+  </section>
 </template>
 
 <script>
