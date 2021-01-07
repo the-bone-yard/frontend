caa1ab448f7f14ea134b649deb053d6db70cf099
Stacy Potten <stacylynnaz@gmail.com>
2021-01-07 10:48:53 -0700

    Remove unnecessary comments and Get Started button

--- src/views/TheHome.vue
+++ src/views/TheHome.vue
@@ -6,9 +6,7 @@
       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrnUwJ9xC5z-HtpYQZc3jHYJfcMtQUf8qzVg&usqp=CAU"
     />
     <h2>Find your pup a play date at the best dog parks around!</h2>
-    <button class="button-get-started">Get Started - woof</button>
     <FindPark />
-    <!-- onClick - take user to search component, TBD -->
   </section>
 </template>
 
