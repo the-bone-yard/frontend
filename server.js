85e2015dc9a4e3a7759fe2af352737d8fa314b4c
Bret Merritt <merrittbret9@gmail.com>
2021-01-07 14:40:22 -0700

    Update server to use express.static

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+const express = require('express');
+const app = express();
 
-const app = express()
+// Serve all the files in '/dist' directory
+app.use(express.static('dist'));
 
-//here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-// this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
-  res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
-
-const port = process.env.PORT || 8080
-app.listen(port)
-console.log(`app is listening on port: ${port}`)
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
