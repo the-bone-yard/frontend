b3e331e8d68ef2a443b85439ddc91386a9f3b280
Stacy Potten <stacylynnaz@gmail.com>
2021-01-06 22:40:02 -0700

    Add input and button for search

---
+++ src/views/FindPark.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <input type="text" v-model="searchTerm" />
+    <button @click="search(searchTerm)">Find a Park!</button>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchTerm: '',
+    };
+  },
+  methods: {
+    search(searchTerm) {},
+  },
+};
+</script>
