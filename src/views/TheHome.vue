923964efab0f415b573b7e0bc168bb402f0fc619
Stacy Potten <stacylynnaz@gmail.com>
2021-01-07 10:25:31 -0700

    Add FindPark to TheHome

--- src/views/TheHome.vue
+++ src/views/TheHome.vue
@@ -1,28 +1,37 @@
 <template>
   <section>
-    <img class='puppy-picture' alt='picture of two puppies in the snow' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrnUwJ9xC5z-HtpYQZc3jHYJfcMtQUf8qzVg&usqp=CAU'/> 
+    <img
+      class="puppy-picture"
+      alt="picture of two puppies in the snow"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrnUwJ9xC5z-HtpYQZc3jHYJfcMtQUf8qzVg&usqp=CAU"
+    />
     <h2>Find your pup a play date at the best dog parks around!</h2>
-    <button class='button-get-started'>Get Started - woof</button>
+    <button class="button-get-started">Get Started - woof</button>
+    <FindPark />
     <!-- onClick - take user to search component, TBD -->
   </section>
 </template>
 
 <script>
+import FindPark from './FindPark.vue';
 
+export default {
+  components: { FindPark },
+};
 </script>
 
 <style scoped>
-  section {
-    margin: 1em;
-    background-color: #AFECEB;
-    padding: 2em;
-  }
-  .puppy-picture {
-    width: 15em;
-    border-radius: 5%;
-  }
-  .button-get-started {
-    border-radius: 20%;
-    padding: 1em;
-  }
+section {
+  margin: 1em;
+  background-color: #afeceb;
+  padding: 2em;
+}
+.puppy-picture {
+  width: 15em;
+  border-radius: 5%;
+}
+.button-get-started {
+  border-radius: 20%;
+  padding: 1em;
+}
 </style>
