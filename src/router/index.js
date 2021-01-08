7f0ff9ea1a021324d9300d616a47416426a3b5c7
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-01-07 18:54:07 -0700

    Remove route object for ResultsItemDetails based on emit interaction

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import TheHome from '../views/TheHome.vue'
 import ResultsContainer from '../views/ResultsContainer.vue'
-import ResultsItemDetails from '../components/ResultsItemDetails.vue'
+// import ResultsItemDetails from '../components/ResultsItemDetails.vue'
 
 Vue.use(VueRouter)
 
@@ -17,12 +17,11 @@ const routes = [
     name: 'Results',
     component: ResultsContainer
   },
-  {
-    path: '/results/item-details',
-    name: 'ItemDetails',
-    component: ResultsItemDetails
-  }
-  //this path for item details will need to be dynamic
+  // {
+  //   path: '/results/item-details',
+  //   name: 'ItemDetails',
+  //   component: ResultsItemDetails
+  // }
 ]
 
 const router = new VueRouter({
