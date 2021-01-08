f88089dadfd13f3422cd6ab096d4c6295e8a2944
Bret Merritt <merrittbret9@gmail.com>
2021-01-08 10:08:42 -0700

    Rename file to match proper naming convensions, add wrapper with propsData and provide

--- tests/unit/ResultListItem.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import ResultsListItem from '@/components/ResultsListItem.vue'
-
-describe('ResultsListItem', () => {
-  it('renders ResultsListItem component', () => {
-    const resultsListItem = mount(ResultsListItem)
-    expect(resultsListItem.exists()).toBe(true)
-  })
-})
