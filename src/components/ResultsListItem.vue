4a2481615ba3e87d457b8d128cd9f36937a1f29f
Bret Merritt <merrittbret9@gmail.com>
2021-01-08 09:48:39 -0700

    Add functionality to display result name and address

---
+++ src/components/ResultsListItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class='item-card'>
+    <h1>{{result.name}}</h1>
+    <h1>{{result.formatted_address}}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['result'],
+}
+</script>
+
+<style>
+
+</style>
