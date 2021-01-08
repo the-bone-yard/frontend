c6a3ddfd07425b06728d0de34d8a34043ea4250b
Nathan Darrington <npdarrington@icloud.com>
2021-01-08 15:47:29 -0700

    Added MyParks cmp to router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import TheHome from '../views/TheHome.vue';
 import ResultsContainer from '../views/ResultsContainer.vue';
+import MyParks from '../components/MyParks.vue'
 
 Vue.use(VueRouter);
 
@@ -16,6 +17,11 @@ const routes = [
     name: 'Results',
     component: ResultsContainer,
   },
+  {
+    path: '/my-parks',
+    name: 'MyParks',
+    component: MyParks
+  }
 ];
 
 const router = new VueRouter({
