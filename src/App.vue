7ad657b21058114bc4c4e4dfbe8e93b65a233f34
Nathan Darrington <npdarrington@icloud.com>
2021-01-09 15:34:09 -0700

    Remove nav styling from App and put global styles on top of selector styles

--- src/App.vue
+++ src/App.vue
@@ -33,31 +33,18 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-
 body,
 html {
   height: 100%;
   padding: 0;
   margin: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: #d6eef2;
 }
 </style>
