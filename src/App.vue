c4fa30fad5581032f144543cad960c812b493eaf
Nathan Darrington <npdarrington@icloud.com>
2021-01-09 11:32:36 -0700

    Remove comments and unneeded declarations

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,13 @@
   <div id="app">
     <TheHeader />
     <!-- make dynamic with :name from park object -->
-      <!-- <ResultsItemDetails /> -->
-    <!-- </router-link> -->
     <router-view/>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue'
-// import ResultsItemDetails from './components/ResultsItemDetails.vue'
-import { bus } from './main'
+import { eventBus } from './main'
 
 export default {
   components: { TheHeader },
@@ -23,16 +20,10 @@ export default {
   provide() {
     return {
       savedParks: this.savedParks,
-      savePark: this.savePark
     }
   },
-  // methods: {
-  //   savePark(newPark) {
-  //     this.savedParks = [...this.savedParks, newPark] 
-  //   }
-  // },
   created() {
-    bus.$on('savePark', (data) => {
+    eventBus.$on('savePark', (data) => {
       this.savedParks.push(data);
     })
   },
