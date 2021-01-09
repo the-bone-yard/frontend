6b593c01e7662ce5cd7a7be674ab5436fdc505fa
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-01-09 15:50:36 -0700

    Access savedParks from Vuex store to display my parks

--- src/components/MyParks.vue
+++ src/components/MyParks.vue
@@ -1,17 +1,17 @@
 <template>
   <section>
     <h1>MyParks Page</h1>
-    <h2 v-if="!savedParks.length">Save a park to view it here!</h2>
+    <h2 v-if="!$store.state.savedParks.length">Save a park to view it here!</h2>
     <article v-else>
       <ol>
-        <li v-for="park in savedParks" :key="park">{{ park }}</li>
+        <li v-for="park in $store.state.savedParks" :key="park">{{ park }}</li>
       </ol>
     </article>
   </section>
 </template>
 
 <script>
-  export default {
-    inject: ['savedParks']
-  }
+export default {
+
+}
 </script>
