6c5471b311435fe313b8273689fc1d1930bac52e
Bret Merritt <merrittbret9@gmail.com>
2021-01-10 12:47:05 -0700

    Refactore getResults to remove proxy

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,5 @@
 export const getResults = (coords) => {
-  return fetch('https://fe-cors-proxy.herokuapp.com', {
-  headers: {
-    "Target-URL": `https://boneyard-be.herokuapp.com/api/coordinates=${coords.latitude},${coords.longitude}`
-  }
-})
-  .then(response => response.json())
-  .catch(error => console.error(error))
+  return fetch(`https://boneyard-be.herokuapp.com/api/coordinates=${coords.latitude},${coords.longitude}`)
+    .then(response => response.json())
+    .catch(error => console.error(error))
 }
