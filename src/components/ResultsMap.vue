eb8eb878094e9503052a5fa229239c4a86ccff86
Bret Merritt <merrittbret9@gmail.com>
2021-01-10 13:58:14 -0700

    Add handleMarkerClick and setSelectedMarker, add results-list-item that renders selected marker as an item

--- src/components/ResultsMap.vue
+++ src/components/ResultsMap.vue
@@ -1,30 +1,37 @@
 <template>
-	<GmapMap
-		:center="centerMap"
-		:zoom="10"
-		map-type-id="terrain"
-		style="width: 100%; height: 50vh"
-	>
-		<GmapMarker
-			v-for="(m, index) in markers"
-			:key="index"
-			:position="m.geometry.location"
-			:clickable="true"
-			:draggable="false"
-			@click="centerMapToClickedLocation(m.name)"
-		/>
-	</GmapMap>
+	<section>
+		<GmapMap
+			:center="centerMap"
+			:zoom="10"
+			map-type-id="terrain"
+			style="width: 100%; height: 50vh"
+		>
+			<GmapMarker
+				v-for="(m, index) in markers"
+				:key="index"
+				:position="m.geometry.location"
+				:clickable="true"
+				:draggable="false"
+				@click="handleMarkerClick(m.name)"
+			/>
+			
+		</GmapMap>
+		<results-list-item :result="this.selectedMarker"></results-list-item>
+	</section>
 </template>
 
 <script>
+import ResultsListItem from './ResultsListItem.vue';
 export default {
+  components: { ResultsListItem },
   props: ['searchResults'],
   data() {
     return {
 			markers: this.searchResults,
 			centerMap: {
 				lat: 40, lng: -105.0465427
-			}
+			},
+			selectedMarker: {}
     }
   },
   methods: {
@@ -37,7 +44,14 @@ export default {
 				};
 			}
 			// make helper method that will open details of park they clicked on
-    }
+		},
+		handleMarkerClick(name) {
+			this.centerMapToClickedLocation(name);
+			this.setSelectedMarker(name);
+		},
+		setSelectedMarker(name) {
+			this.selectedMarker = this.markers.find(marker => name === marker.name)
+		}
   }
 };
 </script>
