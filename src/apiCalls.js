c434bf1990dd63c7a7624f4ee7191d47c9a03787
Bret Merritt <merrittbret9@gmail.com>
2021-01-10 11:16:14 -0700

    pass coords to interpolate into Target-URL

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,10 +1,9 @@
-export const getResults = () => {
+export const getResults = (coords) => {
   return fetch('https://fe-cors-proxy.herokuapp.com', {
   headers: {
-    "Target-URL": "https://boneyard-be.herokuapp.com/api/coordinates=40.560845,-105.094427/"
+    "Target-URL": `https://boneyard-be.herokuapp.com/api/coordinates=${coords.latitude},${coords.longitude}`
   }
 })
   .then(response => response.json())
   .catch(error => console.error(error))
 }
-
