c150ebee701b4720800ba558139b2bffb9725a19
Nathan Darrington <npdarrington@icloud.com>
2021-01-09 17:15:16 -0700

    Added styling for logo in boneyard header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,28 +1,43 @@
 <template>
   <section>
     <article class="bone-yard-header">
-      <h1>Bone Yard</h1>
-      <img
-        class="bone-yard-logo"
-        alt="Bone Yard pawprint logo"
-        src="https://images-na.ssl-images-amazon.com/images/I/31jVeYUkbhL._AC_SY550_.jpg"
-      />
+      <router-link to='/'>
+        <img
+          class="bone-yard-logo"
+          alt="Bone Yard pawprint logo"
+          src="../assets/paw_prints.png"
+        />
+      </router-link>
+      <router-link to='/' class="title-link">
+        <h1 class="bone-yard-font">The<br />Bone Yard</h1>
+      </router-link>
     </article>
     <nav>
-      <router-link to="/">home</router-link> <br />
-      <!-- might not need the home link here - click on bone yard title to go home -->
-      <router-link to="/my-parks">my parks</router-link> <br />
+      <router-link to="/my-parks">My Parks</router-link> <br />
     </nav>
   </section>
 </template>
 
 <style scoped>
-.bone-yard-logo {
-  width: 3em;
+.bone-yard-font {
+  font-family: 'Potta One', cursive;
 }
+
+.bone-yard-logo {
+  width: 2.5em;
+  height: 100%;
+  margin-right: 1em;
+}
+
 .bone-yard-header {
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
+  align-items: center;
   padding: 1em;
+}
+
+.title-link {
+  text-decoration: none;
+  color: black;
 }
 </style>
