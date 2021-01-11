48029d09de0a838b944521dfc4b564154d049809
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-01-11 16:42:46 -0700

    Add functionality for setTimeout and loading message based on results

--- src/components/ResultsList.vue
+++ src/components/ResultsList.vue
@@ -1,8 +1,8 @@
 <template>
   <section class='results-list'>
     <h4 v-if="!$store.state.searchResults.length">
-      <h5>No results found</h5>
-      <FindPark />
+      <h5> {{ message }}</h5>
+      <FindPark v-if="noResults" />
     </h4>
     <section v-else :key='i' v-for='(result, i) in searchResults'>
       <results-list-item :result='result'></results-list-item>
@@ -15,6 +15,30 @@ import ResultsListItem from './ResultsListItem.vue';
 import FindPark from './FindPark.vue';
 export default {
   props: ['searchResults'],
-  components: { ResultsListItem, FindPark }
+  data() {
+    return {
+      noResults: false
+    }
+  },
+  methods: {
+    load() {
+      if (!this.$store.state.searchResults.length) {
+        this.noResults = true
+      }
+    }
+  },
+  mounted() {
+    setTimeout(() => this.load(), 6000)
+  },
+  components: { ResultsListItem, FindPark },
+  computed: {
+    message() {
+      if (this.noResults) {
+        return 'Sorry, no results found. Try a different search!'
+      } else {
+        return'Loading parks...'
+      }
+    }
+  }
 }
 </script>
