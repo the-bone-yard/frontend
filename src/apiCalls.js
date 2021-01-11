096c45b9eb5035bf284ff081301158f6b9619f09
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-01-11 07:38:23 -0700

    Create getPhoto for additional request per Google Places Photo

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,3 +7,9 @@ export const getResults = (coords) => {
   .then(response => response.json())
   .catch(error => console.error(error))
 }
+
+export const getPhoto = (reference) => {
+  return fetch(`https://maps.googleapis.com/maps/api/place/photo?photoreference=${reference}&key=${process.env.VUE_APP_GOOGLE_MAPS_API_KEY}`)
+  .then(res => res.json())
+  .catch(err => console.error(err))
+}
