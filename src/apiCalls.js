b69520581599808588bd49ea6ea2f9bb216c94b6
Bret Merritt <merrittbret9@gmail.com>
2021-01-12 14:27:33 -0700

    Add getSearch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -15,6 +15,12 @@ export const getResults = (coords) => {
 //   .catch(err => console.error(err))
 // }
 
+export const getSearch = (searchTerm) => {
+  return fetch(`https://boneyard-be.herokuapp.com/api/coordinates=${searchTerm}`)
+    .then(response => response.json())
+    .catch(error => console.error(error))
+}
+
 export const getPhoto = (reference) => {
   return fetch(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${reference}&key=${process.env.VUE_APP_GOOGLE_MAPS_API_KEY}`)
   .catch(err => console.error(err))
