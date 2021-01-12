0de5b787685e3aebfce77bb221d2316497359c13
Bret Merritt <merrittbret9@gmail.com>
2021-01-12 16:02:47 -0700

    Move directions rendering functionality to stand-alone component

---
+++ src/components/Directions.vue
@@ -0,0 +1,40 @@
+<template>
+  <ol v-if="this.directions.length">
+    <li :key='i' v-for="(direction, i) in directions">
+      {{direction}}  
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  props: ["park"],
+  data() {
+    return {
+      directions: []
+    }
+  },
+  mounted() {
+    // This is where the apiCall for getting directions will happen 
+    // For now there is placeholder functionality for testing purposes
+    this.directions = [
+        "Up",
+        "Up",
+        "Down",
+        "Down",
+        "Left",
+        "Right",
+        "Left",
+        "Right",
+        "B",
+        "A"
+      ]
+    // apiCall functionality might look like this:
+    // this.directions = getDirections(this.park)
+  }
+}
+</script>
+
+<style>
+
+</style>
