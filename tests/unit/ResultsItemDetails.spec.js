04886053d85f8779fd3168198d2c96a591fe2d1c
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-01-12 10:40:13 -0700

    Add route to unit test

--- tests/unit/ResultsItemDetails.spec.js
+++ tests/unit/ResultsItemDetails.spec.js
@@ -10,7 +10,32 @@ describe('ResultsItemDetails', () => {
   beforeEach(() => {
     state = {
       savedParks: [],
-      searchResults: [],
+      searchResults: [
+        {
+					formatted_address: '8270 Lexington Dr, Colorado Springs',
+					geometry: {
+						location: { lat: 38.952039, lng: -104.7748943 },
+						viewport: {
+							northeast: { lat: 38.95328347989272, lng: -104.7738461701073 },
+							southwest: { lat: 38.95058382010728, lng: -104.7765458298927 },
+						},
+					},
+					name: 'BoneYard',
+					opening_hours: { open_now: true },
+					photos: [
+						{
+							height: 2988,
+							html_attributions: [
+								'<a href="https://maps.google.com/maps/contrib/115566010039288396420">Melodie Marvel</a>',
+							],
+							photo_reference:
+								'ATtYBwKlEoKnkz5JXr2PXbPZ6hkKSQi1vghFDNriwfubSuyxs1CnwgUwMfjXpn4KOOo3sjFqQxGxmwQwx07MiT-apmrrz20QEpBFSA7SWjm4iysxJbiEzhpi6hS3QZzjxUmv_T6z2H4cZcrDRXS404uoQ1cF3M-59L_ExteZXRRaMLirLiDj',
+							width: 5312,
+						},
+					],
+					rating: 4.4,
+				}
+      ],
       geolocation: null
     }
 
@@ -30,8 +55,26 @@ describe('ResultsItemDetails', () => {
     }
   })
 
-  test('should render the component', () => {
-    const wrapper = shallowMount(ResultsItemDetails, { store, localVue, computed })
+  it('should render the component', () => {
+    const $route = {
+      path: '/results/BoneYard',
+      params: {
+        name: 'BoneYard'
+      }
+    }
+    const wrapper = shallowMount(ResultsItemDetails, { 
+      store, 
+      localVue, 
+      computed,
+      mocks: {
+        $route
+      },
+      data() {
+        return {
+          parkName: 'BoneYard'
+        }
+      }
+    })
     expect(wrapper.exists()).toBe(true)
   })
 })
