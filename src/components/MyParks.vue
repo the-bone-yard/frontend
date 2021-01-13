3aea9f5ac405e46a8621b966a88ce20c6a956256
Nathan Darrington <npdarrington@icloud.com>
2021-01-13 11:19:39 -0700

    Add a section around MyParks component for styling

--- src/components/MyParks.vue
+++ src/components/MyParks.vue
@@ -1,12 +1,14 @@
 <template>
-  <section class="saved-list">
+  <section>
     <h1>My Saved Parks</h1>
-    <h2 v-if="!$store.state.email">Sign in with your email to view your saved parks.</h2>
-    <h2 v-else-if="!$store.state.savedParks.length">Save a park to view it here!</h2>
-    <results-list-item v-else 
-      v-for="(result, i) in $store.state.savedParks" 
-      :key='i' 
-      :result='result'></results-list-item>
+    <section class="saved-list">
+      <h2 v-if="!$store.state.email">Sign in with your email to view your saved parks.</h2>
+      <h2 v-else-if="!$store.state.savedParks.length">Save a park to view it here!</h2>
+      <results-list-item v-else 
+        v-for="(result, i) in $store.state.savedParks" 
+        :key='i' 
+        :result='result'></results-list-item>
+    </section>
   </section>
 </template>
 
