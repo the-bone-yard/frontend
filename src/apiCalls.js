d98d3b6258e30efabdfa0ca78c5cfed80cea6536
Bret Merritt <merrittbret9@gmail.com>
2021-01-12 19:33:45 -0700

    Add postSaved

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,3 +9,23 @@ export const getSearch = (searchTerm) => {
     .then(response => response.json())
     .catch(error => console.error(error))
 }
+
+export const postSaved = (parkToSave) => {
+  const convertedPark = {
+    name: parkToSave.name,
+    formatted_address: parkToSave.formatted_address,
+    opening_hours: parkToSave.opening_hours,
+    photo: parkToSave.photo,
+    rating: parkToSave.rating
+  }
+  const init = {
+    method: 'POST',
+    headers: {
+  	  'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(convertedPark)
+  };
+  return fetch(`https://boneyard-be.herokuapp.com/api/park/create`, init)
+    .then(response => response.json())
+    .catch(error => console.error(error))
+}
