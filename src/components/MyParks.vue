eb04bd48444d596ca47cb8da4b61085439e42c3f
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-01-13 10:19:46 -0700

    Add extra condition to prompt user to sign in if there isn't an email

--- src/components/MyParks.vue
+++ src/components/MyParks.vue
@@ -1,7 +1,8 @@
 <template>
   <section>
     <h1>My Saved Parks</h1>
-    <h2 v-if="!$store.state.savedParks.length">Save a park to view it here!</h2>
+    <h2 v-if="!$store.state.email">Sign in with your email to view your saved parks.</h2>
+    <h2 v-else-if="!$store.state.savedParks.length">Save a park to view it here!</h2>
     <article v-else>
       <section :key='i' v-for='(result, i) in $store.state.savedParks'>
         <results-list-item :result='result'></results-list-item>
