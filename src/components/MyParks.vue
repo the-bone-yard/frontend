0703611d5c0b92e4f1afc95fed2b497ad9ff7b9b
Nathan Darrington <npdarrington@icloud.com>
2021-01-13 08:36:54 -0700

    Added styling for MyParks for larger and smaller screens

--- src/components/MyParks.vue
+++ src/components/MyParks.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section class="saved-list">
     <h1>My Saved Parks</h1>
     <h2 v-if="!$store.state.savedParks.length">Save a park to view it here!</h2>
     <results-list-item v-else 
@@ -15,3 +15,13 @@ export default {
   components: { ResultsListItem }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '../styles/_mixins.scss';
+.saved-list {
+  @include customDisplayFlex(0, 0);
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+</style>
