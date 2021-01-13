e8063756bbaa65ab3db1d77a51c84ce02e2f73fc
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-01-13 13:27:52 -0700

    Create getDirections() GET request that takes in coordinates

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -59,3 +59,9 @@ export const getSaved = (email) => {
   })
   .catch(error => console.error(error))
 }
+
+export const getDirections = (myCoords, parkCoords) => {
+  return fetch(`https://boneyard-be.herokuapp.com/api/current=${myCoords.latitude},${myCoords.longitude}/to=${parkCoords.lat},${parkCoords.lng}`)
+  .then(res => res.json())
+  .catch(err => console.error(err))
+}
