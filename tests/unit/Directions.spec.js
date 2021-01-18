f5bb157714181da333041a3725ce5f8f66972170
Nicole Gooden <nicolemgooden.dev@gmail.com>
2021-01-18 12:23:59 -0700

    Create unit tests for rendering of directions based on resolved value

--- tests/unit/Directions.spec.js
+++ tests/unit/Directions.spec.js
@@ -10,20 +10,21 @@ localVue.use(VueRouter)
 localVue.use(Vuex)
 const router = new VueRouter()
 
-describe('Directions', () => {
+describe('Directions happy path', () => {
   let propsData, store, directions
 
   beforeEach(() => {
+
     getDirections.mockResolvedValue({
-      narratives: [
-        'Start here',
-        'Turn left',
-        'Merge onto highway',
-        'Take exit 8',
-        'Destination is on the left'
-      ]
+        narratives: [
+          'Start here',
+          'Turn left',
+          'Merge onto highway',
+          'Take exit 8',
+          'Destination is on the left'
+        ]
     })
-    
+
     propsData = {
       park: {
         formatted_address: 'Address',
@@ -52,16 +53,64 @@ describe('Directions', () => {
     })
 
     directions = shallowMount(Directions, {
-      localVue,
-      store,
-      directions,
-      router,
-      propsData  
+        localVue,
+        store,
+        router,
+        propsData  
     })
   })
 
   it('should render Directions', () => {
     expect(directions.exists()).toBe(true)
+    const allPTags = directions.findAll('p')
+    expect(allPTags.at(0).text()).toBe('Start here')
+    expect(allPTags.at(1).text()).toBe('Turn left')
+    expect(allPTags.at(2).text()).toBe('Merge onto highway')
+    expect(allPTags.at(3).text()).toBe('Take exit 8')
+    expect(allPTags.at(4).text()).toBe('Destination is on the left')
+  })
+})
+
+describe('Directions sad path', () => {
+  it('should show a message if there are no directions', () => {
+    getDirections.mockResolvedValue({narratives: []})
+    
+    const propsData = {
+        park: {
+          formatted_address: 'Address',
+          geometry: {
+            location: {
+              lat: 39.8846317,
+              lng: -105.1379952
+            },
+          name: 'Park Name',
+          opening_hours: {
+            open_now: true
+          },
+          photos: [],
+          rating: 4.9
+          } 
+        }
+      }
+  
+      const store = new Vuex.Store({
+        state: {
+          geolocation: {
+            latitude: 38.9865120,
+            longitude: -105.4524687
+          }
+        }
+      })
+  
+      const directions = shallowMount(Directions, {
+          localVue,
+          store,
+          router,
+          propsData  
+      }) 
+      
+    expect(directions.exists()).toBe(true)
+    expect(directions.find('p').text()).toBe('No directions available.')
   })
 })
 
